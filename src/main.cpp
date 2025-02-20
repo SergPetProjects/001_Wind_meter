2f1712535086158fa0f116d7f7d49dd53255c281
SergGrebnev <serggrebnev79@gmail.com>
2025-02-20 23:41:43 +0700

    Выборка азимута

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,11 @@
 #include <Wire.h>              //  Подключаем библиотеку для работы с шиной I2C
 #include <LiquidCrystal_I2C.h> //  Подключаем библиотеку для работы с LCD дисплеем по шине I2C
 
-#define _MEASUREMENT_PERIOD 10000 // Период измерения (мс)
-#define _ARRAY_VOLUME 60          // Размер массива для хранения скорости ветра (60 = 10 мин / 10000 мс)
+#define _MEASUREMENT_PERIOD 10000   // Период измерения скорости ветра(10 с)
+#define _MEASUREMENT_AZIMUTH 120000 // Период вычисления азимута флюгарки(2 мин)
+#define _ARRAY_PERIOD_VOLUME 60     // Размер массива для хранения скорости ветра (60 = 10 мин / 10000 мс)
+#define _ARRAY_FAZA_VOLUME 600      // Размер массива для хранения азимута
+#define _STEP 5                     // Разбиение по азимутам с шагом 5 градусов
 
 LiquidCrystal_I2C lcd(0x27, 20, 4); //  Объявляем  объект библиотеки, указывая параметры дисплея (адрес I2C = 0x27, количество столбцов = 20, количество строк = 4)
 
@@ -15,16 +18,20 @@ void faza();
 void bubbleSort();
 void lcdFirst();
 void lcdPrint();
+void azimuthSort();
+void azimuth();
 
-volatile bool period_flag = false; // разрешение на измерение периода
-volatile bool faza_flag = false;   // разрешение на измерение фазы
-volatile bool sort_flag = true;    // разрешение на сортировку массива
-volatile uint32_t tmr_period = 0, tmr_old = 0, tmr_faza;
+volatile bool period_flag = true; // разрешение на измерение периода
+volatile bool faza_flag = false;  // разрешение на измерение фазы
+// volatile bool sort_flag = true;    // разрешение на сортировку массива
+volatile uint32_t tmr_period = 0, tmr_old = 0, tmr_faza, tmr_faza_old;
 volatile uint32_t period_time, period_middle, period_max;
-volatile uint16_t counter = 0, azimuth_faza = 0;
-uint32_t period_array[_ARRAY_VOLUME], sort_array[_ARRAY_VOLUME];
-uint32_t tmr = 0;
-uint8_t i = 0;
+volatile uint16_t counter = 0;
+uint32_t period_array[_ARRAY_PERIOD_VOLUME], sort_array[_ARRAY_PERIOD_VOLUME];
+uint32_t azimuth_array[_ARRAY_FAZA_VOLUME], azimuth_faza[_ARRAY_FAZA_VOLUME];
+uint32_t tmr2 = 0, tmr10 = 0;
+uint16_t azimuth_selection[360 / _STEP]; // 360 градусов разбито по 5 градусов
+uint16_t i_period = 0, i_faza = 0, i_faza_max = 0, azimuth_middle = 0;
 
 void setup()
 {
@@ -38,20 +45,32 @@ void setup()
 
 void loop()
 {
-  if (millis() - tmr >= _MEASUREMENT_PERIOD)
+  if (millis() - tmr10 >= _MEASUREMENT_PERIOD)
   {
     period(); // Расчёт периода вращения вертушки
-    tmr = millis();
-    period_flag = true; // разрешает запись времени прихода импульса от D2
+    tmr10 = millis();
+    // period_flag = true; // разрешает запись времени прихода импульса от D2
 
-    period_array[i] = period_time; // заполнение массива данными о периоде (затирается самое старое)
-    if (++i >= _ARRAY_VOLUME)
-      i = 0;
+    period_array[i_period] = period_time; // заполнение массива данными о периоде (затирается самое старое)
+    if (++i_period >= _ARRAY_PERIOD_VOLUME)
+      i_period = 0;
 
-    if (sort_flag)
-      bubbleSort();
+    // if (sort_flag)
+    memcpy(sort_array, period_array, _ARRAY_PERIOD_VOLUME * sizeof(period_array[0])); // копирует массив period_array в sort_array
+    bubbleSort();
+    period_middle = sort_array[_ARRAY_PERIOD_VOLUME / 2];
+    period_max = sort_array[_ARRAY_PERIOD_VOLUME - 3];
 
-    lcdPrint(); //вывод на LCD
+    lcdPrint(); // вывод на LCD
+  }
+
+  if (millis() - tmr2 >= _MEASUREMENT_AZIMUTH)
+  {
+    i_faza_max = i_faza;
+    memcpy(azimuth_array, azimuth_faza, i_faza_max * sizeof(azimuth_faza[0])); // копирует актуальную часть массива azimuth_faza в azimuth_array
+    i_faza = 0;
+    azimuthSort();
+    tmr2 = millis();
   }
 }
 
@@ -63,12 +82,10 @@ void isr()
     tmr_period = micros();
     period_flag = false;
     faza_flag = true; // Включается ожидание импульса от D3 для вычисления фазы (азимута)
-    Serial.print("Время импульса D2 = ");
-    Serial.println(tmr_period);
+    counter++;        // импульсы считаем тут
+    Serial.print("Количество импульсов за 10 сек. = ");
+    Serial.println(counter);
   }
-  counter++; // импульсы считаем тут
-  Serial.print("Количество импульсов = ");
-  Serial.println(counter);
 }
 
 // Расчёт периода вращения вертушки
@@ -82,8 +99,8 @@ void period()
   period_time = (tmr1 - tmr_old) / counter; // Расчёт периода вращения вертушки
   counter = 0;
   tmr_old = tmr1;
-  Serial.print("Период вращения вертушки = ");
-  Serial.println(period_time / 1000000.0);
+  Serial.print("Период вращения вертушки (мс) = ");
+  Serial.println(period_time / 1000.0);
   Serial.println();
 }
 
@@ -93,25 +110,55 @@ void faza()
 {
   if (faza_flag)
   {
+    tmr_faza_old = tmr_faza;
     tmr_faza = micros();
-    azimuth_faza = 360 * (tmr_faza - tmr_period) / period_time; // азимут в градусах
-    Serial.print("Время импульса D3 = ");
-    Serial.println(tmr_faza);
-    Serial.print("Азимут флюгарки = ");
-    Serial.println(azimuth_faza);
-    Serial.println();
+    azimuth(); // Расчёт азимута флюгарки
+    period_flag = true;
     faza_flag = false;
   }
+}
+
+// Расчёт азимута флюгарки
+void azimuth()
+{
+  azimuth_faza[i_faza] = (359 * (tmr_faza - tmr_period)) / (tmr_faza - tmr_faza_old); // азимут в градусах
+  Serial.print("Азимут флюгарки = ");
+  Serial.println(azimuth_faza[i_faza]);
+  Serial.println();
+  if (azimuth_faza[i_faza] <= 359)
+    i_faza++; // для затирания неверного значения азимута на следующей итерации
+}
+
+// Выборка из массива фаз самого частого значения азимута с дискретностью 5 градусов
+void azimuthSort()
+{
+  int max = 0;
+  for (int i = 0; i < 72; i++)
+    azimuth_selection[i] = 0; // обнуление массива
+
+  for (int i = 0; i < i_faza_max; i++)
+  {
+    uint32_t select = azimuth_array[i] / _STEP;
+    if (max < ++azimuth_selection[select])
+    {
+      max = azimuth_selection[select]; // поиска max в массиве
+      azimuth_middle = select * _STEP;
+      Serial.print("azimuth_middle = ");
+      Serial.println(azimuth_middle);
+    }
+    Serial.print(select * _STEP);
+    Serial.print(" - ");
+    Serial.println(azimuth_selection[select]);
+    }
 }
 
 // Сортировка массива (пузырьковая)
 void bubbleSort()
 {
-  sort_flag = false;
-  memcpy(sort_array, period_array, _ARRAY_VOLUME * 4); // копирует массив period_array в sort_array
-  for (int j = 0; j + 1 < _ARRAY_VOLUME; ++j)
+  // sort_flag = false;
+  for (int j = 0; j + 1 < _ARRAY_PERIOD_VOLUME; ++j)
   {
-    for (int i = 0; i + 1 < _ARRAY_VOLUME - j; ++i)
+    for (int i = 0; i + 1 < _ARRAY_PERIOD_VOLUME - j; ++i)
     {
       if (sort_array[i] > sort_array[i + 1])
       {
@@ -121,13 +168,13 @@ void bubbleSort()
       }
     }
   }
-  period_middle = sort_array[_ARRAY_VOLUME / 2];
-  period_max = sort_array[_ARRAY_VOLUME - 3];
-  sort_flag = true;
+
+  // sort_flag = true;
 }
 
 // Первоначальный вывод на экран LCD
-void lcdFirst(){
+void lcdFirst()
+{
   lcd.init(); // initialize the lcd
   lcd.backlight();
   lcd.setCursor(7, 0); //  Устанавливаем курсор в позицию (столбец, строка)
@@ -145,11 +192,16 @@ void lcdFirst(){
 }
 
 // Вывод информации на экран LCD
-void lcdPrint(){
+void lcdPrint()
+{
   lcd.setCursor(8, 0); //  Устанавливаем курсор в позицию (столбец, строка)
   lcd.print(period_middle);
+  lcd.print("    ");
   lcd.setCursor(8, 1); //  Устанавливаем курсор в позицию (столбец, строка)
   lcd.print(period_max);
+  lcd.print("  ");
+  lcd.print(5 / 3);
   lcd.setCursor(8, 2); //  Устанавливаем курсор в позицию (столбец, строка)
-  lcd.print(azimuth_faza);
+  lcd.print(azimuth_middle);
+  lcd.print("    ");
 }
